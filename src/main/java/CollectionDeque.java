563644ab4c645f59a652149c3b915d9dbf6071f8
shirleyndou <shirley.ndou@umuzi.org>
2020-04-03 11:13:51 +0200

    collections redone

--- src/main/java/CollectionDeque.java
+++ src/main/java/CollectionDeque.java
@@ -1,62 +1,44 @@
-import java.util.HashMap;
-import java.util.LinkedList;
+import java.util.*;
 
 public class CollectionDeque {
 
     public static void main(String[] args) {
-        collection(8,4,2,2,2,2,2,2,2,2);
+        collection(9,3,6,2,6,8,3,7,1,4,4);
     }
 
     public static int collection(int intNumber, int subArraySize, int... queue) {
-        LinkedList<Integer> list = new LinkedList<Integer>();
-
-        for (int i = 0; i < intNumber; i++)
-        {
-            list.add(queue[i]); //adding items to the list
-        }
-        System.out.println("intNumber = " + list.size());
-        System.out.println("queue = " + list);
-
         int maxNumbers =0;
+        Deque<Object> deque = new ArrayDeque<>();
 
-        /*A loop to generate all sub arrays of size maxNumbers*/
-
-        for(int i=0; i< intNumber-subArraySize; i++)
+        for (int i = 0; i <= intNumber-1; i++)
         {
-            int unique =0;
+            deque.add(queue[i]); //adding items to the list
+        }
+        System.out.println("intNumber = " + deque.size());
+        System.out.println("queue = " + deque);
 
-            /*A Hash function stores items in key/value pairs
-             * and you can access them by an index of another type
-             * in this case the type will be of LinkedList
-             * LinkedList is my index and Integer will be my object (numbers in the list)*/
+        /*
+        * HashSet extends the Set Interface
+        * Duplicates are not allowed*/
 
-            HashMap <LinkedList, Integer> map = new HashMap<LinkedList, Integer>();
+        HashSet <Integer> hashSet = new HashSet<>();
 
-            for(int j=0; j< i+subArraySize; j++)
-            {
-                /*if the key is new to the map
-                 * push the key to the list and increment by 1*/
+        for(int i=0; i< intNumber-subArraySize; i++) {
+            Object[] temp = deque.toArray(); //takes all the elements in the deque array and copies them to a new array called temp;
 
-                if(!map.containsKey(list))//checking if the key exists in the map
-                {
-                    map.put(list,1); //adding elements in my list
-                    unique++;
-
-                }
-
+            for (int j = 0; j <subArraySize; j++){
+                hashSet.add((Integer) temp[j]); //adding the elements to the temporary array
+        }
+            deque.remove();
+            if(hashSet.size() > maxNumbers){
+                maxNumbers = hashSet.size();
             }
-
-            /*if current unique numbers is greater than the maximum unique numbers
-             * if yes, replace current with max*/
-            if(unique > maxNumbers){
-                maxNumbers = unique;
-            }
-
+            hashSet.clear();// removes the set but does not delete the set
         }
         System.out.println("subArraySize = " + subArraySize); //dear marker, change the subArraySize under the main function
         System.out.println("Output = " + maxNumbers);
 
-         return maxNumbers;
+        return maxNumbers;
     }
 
 
